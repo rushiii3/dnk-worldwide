2ca545b33ec31f2afb29dc1fe942fd87c1c8ddd0
rushiii3 <hrushiop@gmail.com>
2025-05-15 13:57:04 +0530

    added api edit

--- client/src/apiCalls/index.ts
+++ client/src/apiCalls/index.ts
@@ -2,26 +2,28 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const apiClient = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-  }
+  },
+  withCredentials: true, 
 });
 
 // Request interceptor for adding auth token
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// Currently we dont need to store token in local storage
+// apiClient.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('authToken');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 // Response interceptor for handling errors
 apiClient.interceptors.response.use(
